9b89ac5423eefdc0503dfa9958480759cbda225c
Elyaaa001 <gabibovaelina75gmail.com>
2024-07-26 16:22:20 +0400

    1

--- main.h
+++ main.h
@@ -2,17 +2,11 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
-int _putchar(char c);
-int printf_string(va_list val):
-int print_1(void);
-int _strlen(char *str);
-<<<<<<< HEAD
-int _strlenc(const char *str);
+int printf_string(va_list args):
+int print%(void);
 int printf_char(va_list val);
-int print_bin(va_list val)
-=======
 int print_i(va_list args);
 int print_d(va_list args);
->>>>>>> e756d5ca91ed2a5a51b8ee99f4b3e84e87e24b57
+int(*get_function(char ch))(va_list);
 
 #endif 
