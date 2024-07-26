4f57b3e8aed8f57830b3737433eac115f01a901f
Elyaaa001 <gabibovaelina75gmail.com>
2024-07-26 16:09:41 +0400

    1

--- main.h
+++ main.h
@@ -6,5 +6,8 @@ int _putchar(char c);
 int printf_string(va_list val):
 int print_1(void);
 int _strlen(char *str);
+int _strlenc(const char *str);
+int printf_char(va_list val);
+int print_bin(va_list val)
 
 #endif 
