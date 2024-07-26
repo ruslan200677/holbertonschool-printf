a9418971969564bd8e53296adeb9096f5ee5c908
ruslan200677 <qaqli20033@gmail.com>
2024-07-26 13:52:35 +0000

    1

--- allfunctions.c
+++ allfunctions.c
@@ -59,3 +59,57 @@ int print_percent(va_list args)
 
 	return (write(1, "%", 1));
 }
+int print_decimal(va_list args)
+{
+	int digit_count = 1;
+	unsigned int temp;
+
+	int n = va_arg(args, int);
+	unsigned int abs_value;
+	int count;
+
+	if (n < 0)
+	{
+		abs_value = (unsigned int)(-n);
+		count = write(1, "-", 1);
+	}
+
+	else
+	{
+		abs_value = (unsigned int)n;
+		count = 0;
+	}
+
+	if (abs_value == 0)
+	{
+		count += write(1, "0", 1);
+		return (count);
+	}
+
+	for (temp = abs_value; temp > 9; temp /= 10)
+		digit_count *= 10;
+
+	while (digit_count != 0)
+	{
+		char digit_char = ((abs_value / digit_count) % 10) + '0';
+
+		count += write(1, &digit_char, 1);
+		digit_count /= 10;
+	}
+
+	return (count);
+}
+
+/**
+ * print_integer - Function that prints integer
+ *
+ *
+ * Return: Returns 
+ */
+
+int print_integer(va_list args)
+{
+	return (print_decimal(args));
+}
+
+
